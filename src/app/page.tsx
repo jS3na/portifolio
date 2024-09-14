3a57158cc54e68140ff24c81bb55eb44335cebea
js3na <passosjoaogabriel29@gmail.com>
2024-09-14 13:21:28 -0300

    removing unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
 import Projects from "@/components/Projects";
 import Services from "@/components/Services";
-import Image from "next/image";
 
 export default function Home() {
   return (

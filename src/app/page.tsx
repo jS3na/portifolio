8e5f49b9ded4befc3b1cc7db8d2c3711ccbbc174
jS3na <passosjoaogabriel29@gmail.com>
2026-02-16 17:55:07 -0300

    fix: caminhos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import About from "@/components/About";
-import Header from "@/components/Header";
-import HeroSection from "@/components/HeroSection";
-import Projects from "@/components/Projects";
-import Services from "@/components/Services";
+import About from "../components/About";
+import Header from "../components/Header";
+import HeroSection from "../components/HeroSection";
+import Projects from "../components/Projects";
+import Services from "../components/Services";
 
 export default function Home() {
   return (
